e44724ad7405b9376519b5ea7780c25517d78a18
Pratik <khanapurkarpratik@gmail.com>
2025-03-21 20:18:40 +0530

    UI - 6 fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
+import "./navigation.css";
 import Link from "next/link";
 
 const geist = Geist({
